3f846e18c876ab5b93325e4e04cd616a11948f43
jwvk <j.w.vankuilenburg@students.uu.nl>
2021-10-25 12:33:34 +0200

    NLdata function, dataframe withonly NL data

--- data.py
+++ data.py
@@ -4,9 +4,14 @@ def dataloading():
     urlowid= "https://covid.ourworldindata.org/data/owid-covid-data.csv"
     data = pd.read_csv(urlowid)
     print(data)
+    return data
+
+def dataNL(df):
+    NLdata = df.loc[df.location == "Netherlands", "location"]
+    NLdata = pd.merge(NLdata, df)
+    print(NLdata)
+    return NLdata
 
 
-
-
-
-dataloading()
+df = dataloading()
+dataNL(df)
