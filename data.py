4346014c5f4ab5930da8a8db8999903b6560aa82
jwvk <j.w.vankuilenburg@students.uu.nl>
2021-10-25 13:08:43 +0200

    data split into 3 dataframes

--- data.py
+++ data.py
@@ -3,15 +3,25 @@ import pandas as pd
 def dataloading():
     urlowid= "https://covid.ourworldindata.org/data/owid-covid-data.csv"
     data = pd.read_csv(urlowid)
-    print(data)
     return data
 
 def dataNL(df):
     NLdata = df.loc[df.location == "Netherlands", "location"]
     NLdata = pd.merge(NLdata, df)
-    print(NLdata)
     return NLdata
 
+def dataSWE(df):
+    SWEdata = df.loc[df.location == "Sweden", "location"]
+    SWEdata = pd.merge(SWEdata, df)
+    return SWEdata
 
+def dataAUS(df):
+    AUSdata = df.loc[df.location == "Australia", "location"]
+    AUSdata = pd.merge(AUSdata, df)
+    return AUSdata
+
+# create dataframe for each country with data from OurWorldInData
 df = dataloading()
-dataNL(df)
+NLdata = dataNL(df)
+SWEdata = dataSWE(df)
+AUSdata = dataAUS(df)
