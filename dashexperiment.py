62f832507957a87ae55c9a0750be1e2845c19ffb
jwvk <j.w.vankuilenburg@students.uu.nl>
2021-10-27 12:33:42 +0200

    dropdownmenu in dash experiment added is fuctional but very slow to update

--- dashexperiment.py
+++ dashexperiment.py
@@ -1,13 +1,15 @@
 import json
 
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+#import dash_core_components as dcc
+from dash import dcc
+#import dash_html_components as html
+from dash import html
 from dash.dependencies import Input, Output
 import plotly.express as px
 
 import pandas as pd
-
+import numpy as np
 
 #load data function
 def dataloading():
@@ -16,27 +18,43 @@ def dataloading():
     data['date'] = pd.to_datetime(data['date']) #this helps when creating graphs where x = date
     return data
 
+def dataNL(df):
+    NLdata = df.loc[df.location == "Netherlands", "location"]
+    NLdata = pd.merge(NLdata, df)
+    return NLdata
+
 #create data frame for figure
 df = dataloading()
-df1 = df[['date', 'new_cases', 'location']]
-df2 = df[['date', 'stringency_index', 'location']]
+NLdata = dataNL(df)
+
+df1 = NLdata[['date', 'new_cases', 'location', 'stringency_index']]
+df2 = NLdata[['date', 'stringency_index', 'location', 'new_cases']]
 # create graph with data from owid use date as x use new cases for y every 'location' gets different color
-fig = px.line(
-    df1, x=df1['date'], y=df1['new_cases'], 
-    title="corona cases for each country", height=325, color=df['location']
+fig37 = px.line(
+    df1, x='date', y='new_cases',  
+    title="corona cases for each country", height=325
 )
 #create graph 2 specify the dataframe and what xy labels to use, every country gets own color
 fig2 = px.line(
-    df2, x='date', y='stringency_index', 
-    title="stringency for each", height=325, color=df2['location']
+    df2, x='date', y='stringency_index',
+    title="stringency for each", height=325
 )
 
 # initialize dash
 app = dash.Dash(__name__)
 #give layout, graphs set at the top give unique id 
 app.layout = html.Div([
-    dcc.Graph(id="graph", figure=fig),
+    dcc.Graph(id="graph", figure=fig37),
     dcc.Graph(id="graph2", figure=fig2),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[
+            {'label': 'Netherlands_cases', 'value': 'Netherlands'},
+            {'label': 'Netherlands_index', 'value': 'Netherlands_index'}],
+        value='Netherlands'
+        ),
+    dcc.Graph(id='graph1'),
+    
     #idk what this next thing is
     html.Pre(
         id='structure',
@@ -47,7 +65,21 @@ app.layout = html.Div([
         }
     )
 ])
+@app.callback(Output(component_id='graph1', component_property= 'figure'),
+              [Input(component_id='dropdown', component_property= 'value')])
 
-
+def graph_update(dropdown_value):
+    print(dropdown_value)
+    if dropdown_value == "Netherlands":
+        fig = px.line(
+        df1, x='date', y='new_cases',  
+        title="corona cases for each country", height=325
+        )
+    if dropdown_value == "Netherlands_index":
+        fig = px.line(
+        df2, x='date', y='stringency_index',
+        title="stringency for each", height=325
+        )
+    return fig
 
 app.run_server(debug=True)
