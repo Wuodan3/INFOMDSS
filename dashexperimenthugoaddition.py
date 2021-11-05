7e684596d157821ec6b07db8f8ff3a840787617c
jwvk <j.w.vankuilenburg@students.uu.nl>
2021-11-05 21:06:39 +0100

    hugo performance fix

--- dashexperimenthugoaddition.py
+++ dashexperimenthugoaddition.py
@@ -17,14 +17,18 @@ app=Flask(__name__)
 #load data function
 def dataloading():
     urlowid= "https://covid.ourworldindata.org/data/owid-covid-data.csv"
-    data = pd.read_csv(urlowid)
+    colsowid = ['date','new_cases','stringency_index', 'location']
+    data = pd.read_csv(urlowid, usecols=colsowid)
+    data['new_cases'] = data['new_cases'].replace(np.nan, 0)
+    data = data.astype({"new_cases":np.int64, "stringency_index":np.float64, "location":object})
     data['date'] = pd.to_datetime(data['date']) #this helps when creating graphs where x = date
-    #set NaN to 0
     return data
 
 def RIVMdata():
     urlRIVM = "https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_per_dag.csv"
-    data = pd.read_csv(urlRIVM, sep=';')
+    colsrivm = ['Date_of_publication', 'Total_reported']
+    data = pd.read_csv(urlRIVM, usecols=colsrivm, sep=';')
+    data = data.astype({'Total_reported':np.int64})
     data['date'] = pd.to_datetime(data['Date_of_publication'])
     data.sort_values(by=['date'])
     #Total_reported is postive tests per day
@@ -46,12 +50,18 @@ def dataAUS(df):
     return AUSdata
 
 def predictRIVM(df3):
-    # fit model
+    #AutoReg uses Conditional Maximum Likelihood estimate to predict 
+    #source: https://www.statsmodels.org/dev/generated/statsmodels.tsa.ar_model.AutoReg.html
+    #source: https://www.sciencedirect.com/topics/mathematics/conditional-likelihood 
     model = AutoReg(df3, lags=1)
     model_fit = model.fit()
-    # let's make prediction
     y = model_fit.predict(len(df3), len(df3)+30)
-    print(y)
+    return y
+
+def predictor(x):
+    model = AutoReg(x, lags=1)
+    model_fit = model.fit()
+    y = model_fit.predict(len(x), len(x)+30)
     return y
 
 #create dataframes
@@ -71,11 +81,11 @@ df3 = RIVMdf[['date', 'Total_reported']]
 print(df3.info())
 df3 = df3.groupby('date')['Total_reported'].sum()
 df3 = pd.DataFrame({'date':df3.index, 'Total_reported':df3.values})
+print(df3.info())
 df4 = SWEdata[['date', 'new_cases', 'location', 'stringency_index']]
 df5 = AUSdata[['date', 'new_cases', 'location', 'stringency_index']]
 df6 = threecountries[['date', 'new_cases', 'location', 'stringency_index']]
 
-print(df3.info())
 
 #predict RIVM data using predictRIVM()
 df3onlynums = df3['Total_reported']
@@ -87,65 +97,98 @@ predicteddf = pd.DataFrame({'index':predict.index, 'predicted':predict.values})
 predicteddf['date'] = pd.date_range(start=pd.Timestamp('today'), periods=31)
 #remove hours from column date
 predicteddf['date'] = pd.to_datetime(predicteddf['date']).dt.date
+predicteddf['location'] = 'Netherlands'
 print(predicteddf)
 
-figRIVMpredicted = px.line(
-    predicteddf, x='date', y='predicted',
-    title="Rredicted trend for the next 30 days", height=450
+# AutoRegression for sweden using predictor()
+SWEpredict = SWEdata['new_cases']
+predictSWE = predictor(SWEpredict.astype(float))
+predictedSWE = []
+predictedSWE = pd.DataFrame({'index':predictSWE.index, 'predicted':predictSWE.values})
+predictedSWE['date'] = pd.date_range(start=pd.Timestamp('today'), periods=31)
+predictedSWE['date'] = pd.to_datetime(predictedSWE['date']).dt.date
+predictedSWE['location'] = 'Sweden'
+print(predictedSWE)
+
+# AUtoRegression for Australia using predictor(), could be more efficient probably
+AUSpredict = AUSdata['new_cases']
+predictAUS = predictor(AUSpredict.astype(float))
+predictedAUS = []
+predictedAUS = pd.DataFrame({'index':predictAUS.index, 'predicted':predictAUS.values})
+predictedAUS['date'] = pd.date_range(start=pd.Timestamp('today'), periods=31)
+predictedAUS['date'] = pd.to_datetime(predictedAUS['date']).dt.date
+predictedAUS['location'] = 'Australia'
+print(predictedAUS)
+
+# combine data
+framesPredicted = [predicteddf, predictedSWE, predictedAUS]
+NLSWEAUSpredicted = pd.concat(framesPredicted)
+print(NLSWEAUSpredicted)
+
+
+# create graph with data from owid use date as x use new cases for y every 'location' gets different color
+
+#create graph 2 specify the dataframe and what xy labels to use, every country gets own color
+
+
+figPredicted = px.line(
+    NLSWEAUSpredicted, x='date', y='predicted', color='location',
+    title="Predicted trend for the next 30 days", height=450
 )
+
+
 
 # initialize dash
 app = dash.Dash(__name__)
 #give layout, graphs set at the top give unique id 
 app.layout = html.Div([
+    html.Div([html.H1('COVID-19 Dashboard with Predictive Analytics', style={'font-family':'verdana'})]),
+    dcc.Graph(id="graph", figure=figPredicted),
+#    dcc.Graph(id="graph3", figure=figRIVM),
+    
     dcc.Dropdown(
         id='dropdown',
         options=[
-            {'label': 'Netherlands_cases', 'value': 'Netherlands_cases'},
-            {'label': 'Sweden_cases', 'value': 'Sweden_cases'},
-            {'label': 'Australia_cases', 'value': 'Australia_cases'},
+            {'label': 'Netherlands', 'value': 'Netherlands_cases'},
+            {'label': 'Sweden', 'value': 'Sweden_cases'},
+            {'label': 'Australia', 'value': 'Australia_cases'},
             {'label': 'All', 'value': 'All'}],
-        value='Netherlands_cases'
+        value='All'
         ),
     #instead of plotting specific countries we can plot predicted graphs
     dcc.Graph(id='graph1'),
-    dcc.Graph(id='graph2'),
-    
-    #idk what this next thing is
-    html.Pre(
-        id='structure',
-        style={
-            'border': 'thin lightgrey solid', 
-            'overflowY': 'scroll',
-            'height': '275px'
-        }
-    )
+    dcc.Graph(id='graph2')
 ])
 @app.callback(Output(component_id='graph1', component_property= 'figure'),
               [Input(component_id='dropdown', component_property= 'value')])
-
 
 def graph_update(dropdown_value):
     print(dropdown_value)
     if dropdown_value == "Netherlands_cases":
         fig = px.line(
-        df1, x='date', y='new_cases',  
-        title="corona cases for Netherlands", height=325
+        df3, x='date', y='Total_reported',  
+        title="corona cases for Netherlands", height=450
         ) 
 
     if dropdown_value == "Sweden_cases":
         fig = px.line(
         df4, x='date', y='new_cases',  
-        title="corona cases for Netherlands", height=325
+        title="corona cases for Sweden", height=450
         ) 
         
     if dropdown_value == "Australia_cases":
         fig = px.line(
         df5, x='date', y='new_cases',  
-        title="corona cases for Netherlands", height=325
+        title="corona cases for Australia", height=450
         ) 
+    if dropdown_value == "All":
+        fig = px.line(
+        threecountries, x='date', y='new_cases', color='location',
+        title="corona cases for each country", height=450
+)     
 
     return fig
+
 
 @app.callback(Output(component_id='graph2', component_property= 'figure'),
               [Input(component_id='dropdown', component_property= 'value')])
@@ -155,24 +198,27 @@ def graph_update(dropdown_value):
     if dropdown_value == "Netherlands_cases":
         fig = px.line(
         df1, x='date', y='stringency_index',  
-        title="corona cases for Netherlands", height=325
+        title="Stringency Index for Netherlands", height=450
         )
 
     if dropdown_value == "Sweden_cases":
         fig = px.line(
         df4, x='date', y='stringency_index',  
-        title="corona cases for Netherlands", height=325
+        title="Stringency Index for Sweden", height=450
         ) 
         
     if dropdown_value == "Australia_cases":
         fig = px.line(
         df5, x='date', y='stringency_index',  
-        title="corona cases for Netherlands", height=325
+        title="Stringency Index for Australia", height=450
         ) 
-    
-    return fig
-    
+    if dropdown_value == "All":
+        fig = px.line(
+        threecountries, x='date', y='stringency_index', color='location',
+        title="stringency for each", height=450
+        )
 
+    return fig
 
 if __name__ == "__main__":
-    app.run_server(host='0.0.0.0', debug= True, port=5000)
+    app.run_server(host='0.0.0.0', debug= False, port=5000)
