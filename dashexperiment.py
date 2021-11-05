69e4dcb6d170ebe36ef84a863a54afb7df3db04d
jwvk <j.w.vankuilenburg@students.uu.nl>
2021-11-05 17:22:38 +0100

    changes in graphs

--- dashexperiment.py
+++ dashexperiment.py
@@ -146,8 +146,8 @@ app = dash.Dash(__name__)
 #give layout, graphs set at the top give unique id 
 app.layout = html.Div([
     html.Div([html.H1('COVID-19 Dashboard with Predictive Analytics', style={'font-family':'verdana'})]),
-#    dcc.Graph(id="graph", figure=fig37),
-#    dcc.Graph(id="graph200", figure=fig2),
+    dcc.Graph(id="graph", figure=fig37),
+    dcc.Graph(id="graph200", figure=fig2),
 #    dcc.Graph(id="graph3", figure=figRIVM),
     
     dcc.Dropdown(
